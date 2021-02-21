ba9aefd3a19cb0f7ce96f5192a60d93e4fca933f
jordyalcides <jordy.alcides@gmail.com>
2021-02-21 14:03:03 -0300

    Fixed price not changing when input changed;

--- assets/js/traffic-price-slider.js
+++ assets/js/traffic-price-slider.js
@@ -4,17 +4,17 @@
 // - 500k pageviews / $24 per month
 // - 1M pageviews / $36 per month
 
-const inputToViews = [ "10K", "50K", "100K", "500K", "1M"]
-const inputToPrice = [ "8", "12", "16", "24", "36"]
+const valueToViews = [ "10K", "50K", "100K", "500K", "1M"]
+const valueToPrice = [ "8.00", "12.00", "16.00", "24.00", "36.00"]
 
 let viewsValue = document.getElementById("views")
+let priceAmount = document.getElementById("price-amount")
 const trafficButton = document.getElementById("traffic-button")
-
-// viewsValue.innerHTML = trafficButton.value
 
 trafficButton.oninput = function () {
     const index = Math.ceil(this.value/25)
-    viewsValue.innerHTML = inputToViews[index]
+    viewsValue.innerHTML = valueToViews[index]
+    priceAmount.innerHTML = valueToPrice[index]
 }
 
 trafficButton.addEventListener("touchmove", () => {
